d71d7860b73a0fb9fc0131cb4be7d8c24db91e66
fakhryhizballah <falehry88@gmail.com>
2023-11-16 23:38:08 +0700

    Remove unnecessary code and console logs from controllers/index.js

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,6 @@
 'use strict';
-const { sensors, sequelize } = require('../models');
+const { sensors } = require('../models');
 const { Op } = require("sequelize");
-const moment = require('moment-timezone');
-const timeZone = moment.tz.guess();
-console.log('Zona Waktu:', timeZone);
 module.exports = {
     getAverage: async (req, res) => {
         try {
@@ -37,7 +34,6 @@ module.exports = {
             let average = [];
             for (let i = 0; i < data.length; i++) {
                 let jam = data[i].created_at.slice(11, 13);
-                console.log(jam);
                 // Tanggal dan waktu yang diberikan
                 if (hour == jam) {
                     value1 += data[i].value1;
@@ -48,7 +44,6 @@ module.exports = {
                     count++;
                 } else {
                     hour = jam;
-                    console.log(hour);
                     value1 = data[i].value1;
                     value2 = data[i].value2;
                     value3 = data[i].value3;
@@ -69,108 +64,12 @@ module.exports = {
             for (let i = 0; i < average.length; i++) {
                 //   convert name to string hh:mm 
                 average[i].id = i + 1;
-                // let jam = average[i].name;
-                // if (jam < 10) {
-                //     jam = '0' + jam + ':00';
-                // } else {
-                //     jam = '' + jam + ':00';
-                // }
-                // average[i].name = jam;
-
             }
             return res.status(200).json({
                 status: true,
                 message: 'Success',
                 data: average,
-                // raw: data
-            });
-        } catch (error) {
-            console.log(error);
-            return res.status(500).json({
-                status: false,
-                message: 'Internal Server Error',
-                data: error
-            });
-        }
-    },
-    getAverageDay: async (req, res) => {
-        try {
-            let data = await sensors.findAll({
-                where: {
-                    updated_at: {
-                        [Op.between]: [req.query.start, req.query.end]
-                    }
-                },
-                order: [
-                    ['created_at', 'ASC'],
-                ],
-            });
-
-            // buat rata rata per hari
-            let value1 = 0;
-            let value2 = 0;
-            let value3 = 0;
-            let value4 = 0;
-            let value5 = 0;
-            let count = 0;
-            let day = -1;
-            let tanggal = '';
-            let id = 0;
-            let average = [];
-            for (let i = 0; i < data.length; i++) {
-                let newDate = data[i].created_at;
-                newDate.setHours(newDate.getHours() + 14);
-                data[i].created_at = newDate;
-                data[i].updated_at = newDate;
-                if (day == data[i].created_at.getDate()) {
-                    value1 += data[i].value1;
-                    value2 += data[i].value2;
-                    value3 += data[i].value3;
-                    value4 += data[i].value4;
-                    value5 += data[i].value5;
-                    count++;
-                } else {
-
-                    day = data[i].created_at.getDate();
-                    value1 = data[i].value1;
-                    value2 = data[i].value2;
-                    value3 = data[i].value3;
-                    value4 = data[i].value4;
-                    value5 = data[i].value5;
-                    count = 1;
-                    // get yyyy-mm-dd from data[i].created_at + 14 hours
-                    tanggal = data[i].created_at.toISOString().split('T')[0];
-                
-                    console.log(tanggal);
-                    
-                    average.push({
-                        name: tanggal,
-                        value1: (value1 / count).toFixed(2),
-                        value2: (value2 / count).toFixed(2),
-                        value3: (value3 / count).toFixed(2),
-                        value4: (value4 / count).toFixed(2),
-                        value5: (value5 / count).toFixed(2),
-                    });
-                }
-
-            }
-            for (let i = 0; i < average.length; i++) {
-                //   convert name to string hh:mm 
-                average[i].id = i + 1;
-                let day = average[i].name;
-                if (day < 10) {
-                    day = '0' + day;
-                } else {
-                    day = '' + day;
-                }
-                average[i].name = day;
-
-            }
-            return res.status(200).json({
-                status: true,
-                message: 'Success',
-                data: average,
-                // raw: data
+                raw: data
             });
         } catch (error) {
             console.log(error);
@@ -215,7 +114,6 @@ module.exports = {
             let average = [];
             for (let i = 0; i < data.length; i++) {
                 let daynow = data[i].created_at.slice(0, 2);
-                console.log(day);
                 if (day == daynow) {
                     value1 += data[i].value1;
                     value2 += data[i].value2;
@@ -291,9 +189,7 @@ module.exports = {
             let month;
             let average = [];
             for (let i = 0; i < data.length; i++) {
-                console.log(data[i].created_at);
                 let monthnow = data[i].created_at.slice(3, 5);
-                console.log(month);
                 if (month == monthnow) {
                     value1 += data[i].value1;
                     value2 += data[i].value2;
